33facf8bdbab1800d6c90800509e1cb999ee560e
yaredghebre <yared2391@gmail.com>
2023-06-30 16:47:15 -0300

    Add: Card, Condition 'create rest'

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $restaurant = Restaurant::where('user_id', Auth::user()->id)->get();
+        return view('admin.dashboard', compact('restaurant'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,12 +3,34 @@
 @section('content')
     <div class="container">
         <h2 class="fs-4 text-secondary my-4">
-            {{ __('Dashboard') }}
+            Benvenuto {{ Auth::user()->name }}
         </h2>
         <div class="row justify-content-center">
-            <div class="col">
-                <a href="{{ route('admin.restaurants.create') }}" class="btn btn-primary">Crea un ristorante</a>
-            </div>
+
+            @if (count($restaurant) === 0)
+                <div class="col">
+                    <a href="{{ route('admin.restaurants.create') }}" class="btn btn-primary">Crea un ristorante</a>
+                </div>
+            @else
+                @foreach ($restaurant as $item)
+                    <div class="card">
+
+                        <img src="{{ asset('storage/' . $item->image) }}" class="card-img-top w-50" alt="Restaurant Image">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $item->name }}</h5>
+                            <p class="card-text">Indirizzo: {{ $item->address }}</p>
+                            <p class="card-text">P.IVA: {{ $item->vat_number }}</p>
+                            @if ($item->description)
+                                <p class="card-text">Descrizione: {{ $item->description }}</p>
+                            @else
+                                <p>Descrizione non disponibile</p>
+                            @endif
+                            <a href="#" class="btn btn-primary">Visualizza prodotti</a>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
+
         </div>
     </div>
 @endsection
