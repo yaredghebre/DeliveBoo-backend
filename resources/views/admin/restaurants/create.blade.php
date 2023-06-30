231931c4116dfdb03c905dda07652106d9d1c018
PietroSavini <savinipietro@gmail.com>
2023-06-30 19:10:30 +0200

    ADD: front-end validation on "retaurants/create"

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -5,35 +5,55 @@
 
 
     <div class="containter vh-100">
-        <div class="row h-100 justify-content-center align-items-center">
-            @if ($errors->any())
-                <ul class="allert">
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            @endif
+        @if ($errors->any())
+            <ul class="alert alert-danger w-50 m-auto mt-4 list-unstyled">
+                <h4 class="text-center" >Attenzione</h4>
+                <li class="text-center" >Controlla gli errori nei campi</li>
+            </ul>    
+        @endif
+        <div class="row h-100 justify-content-center py-4">
             <div class="col-6">
                 <form action="{{ route('admin.restaurants.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="mb-3">
                         <label for="restaurant_name" class="form-label">Nome Attività</label>
-                        <input type="text" class="form-control" id="restaurant_name" name="name">
+                        <input type="text" class="form-control @error('name')is-invalid @enderror" id="restaurant_name" name="name" >
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                        
                     </div>
                     <div class="mb-3">
                         <label for="restaurant_address" class="form-label">Indirizzo Attività</label>
-                        <input type="text" class="form-control" id="restaurant_address" name="address">
+                        <input type="text" class="form-control @error('address')is-invalid @enderror" id="restaurant_address" name="address">
+                        @error('address')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                     </div>
                     <div class="mb-3">
-                        <label for="restaurant_vat_number" class="form-label">Numero P.IVA</label>
-                        <input type="text" class="form-control" id="restaurant_vat_number" name="vat_number">
+                        <label for="restaurant_vat_number" class="form-label">P.IVA</label>
+                        <input type="text" class="form-control @error('vat_number')is-invalid @enderror" id="restaurant_vat_number" name="vat_number">
+                        @error('vat_number')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label class="form-label" for="restaurant_img">
                             Seleziona un immagine di copertina per la tua
                             attività
                         </label>
-                        <input type="file" class="form-control" id="restaurant_img" name="image">
+                        <input type="file" class="form-control  @error('image')is-invalid @enderror" id="restaurant_img" name="image">
+                        @error('image')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <div class="form-floating">
