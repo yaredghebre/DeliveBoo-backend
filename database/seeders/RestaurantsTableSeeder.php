c8e76f92102ae17f9a8ab1770770192840f42ec1
giorgiacapponi <giorgiacapponi3@gmail.com>
2023-07-01 15:53:22 +0200

    started restaurant seeder

--- database/seeders/RestaurantsTableSeeder.php
+++ database/seeders/RestaurantsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Restaurant;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class RestaurantsTableSeeder extends Seeder
 {
@@ -13,16 +14,18 @@ class RestaurantsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        for($i=0;$i<20;$i++){
+    public function run(Faker $faker)
+    { 
+        $restaurants=['da Gino','Mamma mia','la Luna','Roma Roma'];
+        foreach($restaurants as $restaurant){
             $new_restaurant=new Restaurant();
+            $new_restaurant->user_id=$faker->randomNumber(count($restaurants));
             $new_restaurant->name='da Gino';
             $new_restaurant->address='via roma';
-            $new_restaurant->vat_number=;
-            $new_restaurant->image='';
-            $new_restaurant->text='blablabla';
+            $new_restaurant->vat_number=$faker->vat();
+            $new_restaurant->save();
 
         }
+        
     }
 }
