5499b168a3fee88e91d0df4a37f8107bd51a636d
giorgiacapponi <giorgiacapponi3@gmail.com>
2023-07-02 13:17:37 +0200

    added types seeder

--- database/seeders/RestaurantsTableSeeder.php
+++ database/seeders/RestaurantsTableSeeder.php
@@ -16,9 +16,9 @@ class RestaurantsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     { 
-        $italian_restaurants=['da Gino','Mamma mia','la Luna','Roma Roma','pizza italia','magica','trattoria','pizzeria ristorante','sora lella','pizza napoli','sushi shiso','sushi brasiliano','sushi roma','sushi sushi','bistecche wow','brace viva','la griglieria'];
+        $restaurants=['da Gino','Mamma mia','la Luna','Roma Roma','pizza italia','magica','trattoria','pizzeria ristorante','sora lella','pizza napoli','sushi shiso','sushi brasiliano','sushi roma','sushi sushi','bistecche wow','brace viva','la griglieria'];
         $number=12345678912;
-        foreach($italian_restaurants as $restaurant){
+        foreach($restaurants as $restaurant){
             $new_restaurant=new Restaurant();
             $new_restaurant->name=$restaurant;
             $new_restaurant->address='via roma';
