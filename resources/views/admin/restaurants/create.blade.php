eb1284d6c70ee46201b2774a204842ce3b9f09a7
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-07-06 12:14:23 +0200

    publish-branc

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -1,15 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-
-
-
     <div class="containter vh-100  restaurants">
         @if ($errors->any())
             <ul class="alert alert-danger w-50 m-auto mt-4 list-unstyled">
-                <h4 class="text-center" >Attenzione</h4>
-                <li class="text-center" >Controlla gli errori nei campi</li>
-            </ul>    
+                <h4 class="text-center">Attenzione</h4>
+                <li class="text-center">Controlla gli errori nei campi</li>
+            </ul>
         @endif
         <div class="row h-100 justify-content-center py-4">
             <div class="col-6">
@@ -17,26 +14,32 @@
                     @csrf
                     <div class="mb-3">
                         <label for="restaurant_name" class="form-label">Nome Attività</label>
-                        <input type="text" minlength="4" maxlength="50" required class="form-control @error('name')is-invalid @enderror" id="restaurant_name" name="name" value="{{ old('name') }}">
+                        <input type="text" minlength="4" maxlength="50" required
+                            class="form-control @error('name')is-invalid @enderror" id="restaurant_name" name="name"
+                            value="{{ old('name') }}">
                         @error('name')
                             <div class="invalid-feedback">
                                 {{ $message }}
                             </div>
                         @enderror
-                        
+
                     </div>
                     <div class="mb-3">
                         <label for="restaurant_address" class="form-label">Indirizzo Attività</label>
-                        <input type="text" minlength="5" maxlength="60"required class="form-control @error('address')is-invalid @enderror" id="restaurant_address" name="address" value="{{ old('address') }}">
+                        <input type="text" minlength="5" maxlength="60"required
+                            class="form-control @error('address')is-invalid @enderror" id="restaurant_address"
+                            name="address" value="{{ old('address') }}">
                         @error('address')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                    @enderror
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="restaurant_vat_number" class="form-label">P.IVA</label>
-                        <input type="text"  minlength="11" maxlength="11" required class="form-control @error('vat_number')is-invalid @enderror" id="restaurant_vat_number" name="vat_number" value="{{ old('vat_number') }}">
+                        <input type="text" minlength="11" maxlength="11" required
+                            class="form-control @error('vat_number')is-invalid @enderror" id="restaurant_vat_number"
+                            name="vat_number" value="{{ old('vat_number') }}">
                         @error('vat_number')
                             <div class="invalid-feedback">
                                 {{ $message }}
@@ -48,7 +51,8 @@
                             Seleziona un immagine di copertina per la tua
                             attività
                         </label>
-                        <input type="file" required class="form-control   @error('image')is-invalid @enderror" id="restaurant_img" name="image" value="{{ old('image') }}">
+                        <input type="file" required class="form-control   @error('image')is-invalid @enderror"
+                            id="restaurant_img" name="image" value="{{ old('image') }}">
                         @error('image')
                             <div class="invalid-feedback">
                                 {{ $message }}
@@ -58,14 +62,14 @@
                     <div class="mb-3">
                         <p>Aggiungi una descrizione della tua attività</p>
                         <div class="form-floating">
-                            <textarea minlength="6" maxlength="1000" class="form-control @error('description')is-invalid @enderror" placeholder="Descrizione Attività" id="restaurant_description" style="height: 160px"
-                                name="description">{{ old('description') }}</textarea>
+                            <textarea minlength="6" maxlength="1000" class="form-control @error('description')is-invalid @enderror"
+                                placeholder="Descrizione Attività" id="restaurant_description" style="height: 160px" name="description">{{ old('description') }}</textarea>
                             <label for="restaurant_description">Descrizione</label>
                             @error('description')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
 
                         </div>
 
@@ -73,22 +77,24 @@
 
                     <p>seleziona le categorie che più si addicono alla tua attività</p>
                     <div class="mb-3 types-checkbox form-tags-container row ">
-                        
+
                         @foreach ($types as $type)
-                        
-                        <div class="form-check form-ceck-tag ">
-                            <input required class="form-check-input ms_form-check @error('types')is-invalid @enderror  checkTypes" type="checkbox" value="{{$type->id}}" id="type-{{$type->id}}" name="types[]" @checked(in_array($type->id,old('types',[])))>
-                            <label class="form-check-label absolute " for="type-{{$type->id}}">
-                                {{$type->name}}
-                            </label>
-                            
-                        </div>
+                            <div class="form-check form-ceck-tag ">
+                                <input
+                                    class="form-check-input ms_form-check @error('types')is-invalid @enderror  checkTypes"
+                                    type="checkbox" value="{{ $type->id }}" id="type-{{ $type->id }}"
+                                    name="types[]" @checked(in_array($type->id, old('types', [])))>
+                                <label class="form-check-label absolute " for="type-{{ $type->id }}">
+                                    {{ $type->name }}
+                                </label>
+
+                            </div>
                         @endforeach
-                   
-                        @error('types')   
-                            <p class="checkbox-error ">seleziona almeno una categoria</p>   
+
+                        @error('types')
+                            <p class="checkbox-error ">seleziona almeno una categoria</p>
                         @enderror
-                        
+
                     </div>
 
                     <div class="text-center">
@@ -98,4 +104,7 @@
                 </form>
             </div>
         </div>
+    @endsection
+    @section('script')
+        @vite(['resources/js/helpers/checkedBoxValidation.js'])
     @endsection
