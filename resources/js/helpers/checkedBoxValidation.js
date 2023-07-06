955590c07df9f98e4ef14e874c79caa6c3b8bea9
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-07-06 12:59:26 +0200

    add: checkedBoxValidation scrip

--- resources/js/helpers/checkedBoxValidation.js
+++ resources/js/helpers/checkedBoxValidation.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const button = document.getElementById('restaurant-form');
     const msgError = document.getElementById('category-error');
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    console.log(button, checkboxes);
     button.addEventListener('click', function (event) {
         let checkedOne = Array.prototype.slice.call(checkboxes).some(x => x.checked);
         msgError.classList.add('d-none');
