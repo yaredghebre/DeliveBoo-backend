c1ac574f402979cca4b2671755f143a359c20be9
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-07-10 22:35:58 +0200

    TO DO, handle total price,  server side

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PaymentRequest;
+use App\Models\Order;
+use App\Models\OrderProduct;
 use Braintree\Gateway;
 use Illuminate\Http\Request;
 
@@ -29,15 +31,36 @@ class PaymentController extends Controller
             ],
         ]);
         if ($result->success) {
+            $data = $request->all();
+            $orderMessage = 'Data has been saved';
+            //
+            //devo creare un ordine
+            $order = new Order();
+            $order->fill($data);
+            $order->save();
+
+            foreach ($data['product_id'] as $index => $product_id) {
+                $order_product = new OrderProduct();
+                $order_product->order_id = $order->id;
+                $order_product->product_id = $product_id;
+                $order_product->product_quantity = $data['quantity'][$index];
+                $order_product->save();
+            }
+            if (!($order && $order_product)) {
+
+                $orderMessage = 'Data not saved';
+            }
             $data = [
                 'message' => 'Transazione Approvata',
                 'success' => true,
+                'data_confirmation' => $orderMessage
             ];
             $confNumb = 200;
         } else {
             $data = [
                 'message' => 'Transazione Rifiutata',
-                'success' => false
+                'success' => false,
+                'data_confirmation' => 'Data not saved'
             ];
             $confNumb = 401;
         }
