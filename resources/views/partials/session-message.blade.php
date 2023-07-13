501688f15ddd9f2189cd9353cc25486be567ec6b
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-07-13 19:53:10 +0200

    feat: :sparkles: Adds: centered banner for editing products

--- resources/views/partials/session-message.blade.php
+++ resources/views/partials/session-message.blade.php
@@ -1,7 +1,6 @@
 @if (session('message'))
-    <div class="container w-50 ms_alert_handle z-1 position-absolute ">
-
-        <div class="alert alert-success text-center">
+    <div class="container w-50 ms_alert_handle z-1 position-relative">
+        <div class="ms_popup-message alert alert-success text-center position-absolute">
             <strong> {{ session('message') }}</strong>
         </div>
     </div>
