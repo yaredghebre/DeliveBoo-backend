79da01719f7fce02b929208c321321c714b6b19f
PietroSavini <savinipietro@gmail.com>
2023-07-15 14:27:41 +0200

    style of session message

--- resources/views/partials/session-message.blade.php
+++ resources/views/partials/session-message.blade.php
@@ -2,7 +2,7 @@
     <div class="container ms_alert_handle z-600 position-relative">
         <div class="ms_popup-message alert alert-success text-center position-absolute">
             <img src="{{asset('img/success.gif')}}" alt="">
-            <div class="inline-block">
+            <div class="d-flex align-items-center">
                 <strong> {{ session('message') }}</strong>
             </div>
         </div>
