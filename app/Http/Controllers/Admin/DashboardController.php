55386c416c3a80e7c9ad4145fdadd75436ad8bba
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-07-17 12:58:36 +0200

    refactor code

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,7 +10,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $restaurant = Auth::user()->restaurant;
+        $restaurant = Auth::user()->restaurant->with('categories');
         $orders = [];
         $products = [];
         if ($restaurant) {
