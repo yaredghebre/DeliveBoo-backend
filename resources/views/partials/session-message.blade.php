fce67a2f2a5a90db2f30f3840e2490320ee306de
PietroSavini <savinipietro@gmail.com>
2023-07-17 18:59:18 +0200

    modified error on session message

--- resources/views/partials/session-message.blade.php
+++ resources/views/partials/session-message.blade.php
@@ -1,11 +1,10 @@
 @if (session('message'))
-    <div class="container ms_alert_handle z-600 position-relative">
+    <div class="container ms_alert_handle z-600 position-absolute">
         <div class="ms_popup-message alert alert-success text-center position-absolute">
             <img src="{{asset('img/success.gif')}}" alt="">
             <div class="d-flex align-items-center">
                 <strong> {{ session('message') }}</strong>
             </div>
         </div>
-        
     </div>
 @endif
